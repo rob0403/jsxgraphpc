c22eabdb3492dc1f18b3141ada6a9abe33b2ef73
rob0403 <rob.falkenstein@rz.uni-freiburg.de>
2022-12-22 12:06:54 +0100

    update plugin.php

--- plugin.php
+++ plugin.php
@@ -9,12 +9,12 @@
 $id = 'jsxgraph';
 
 // code version; must be changed for all code changes
-$version = '0.0.1';
+$version = '0.0.2';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = '5.1.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '7.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Per Pascal Grube';
